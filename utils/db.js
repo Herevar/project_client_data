2194db5447e2ef254f13241d47f8838dc99fd28d
herevar <laghbert@gmail.com>
2023-03-14 13:51:58 +0000

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -12,6 +12,10 @@ class Db {
     async _load(){
         this._data = JSON.parse(await readFile(this.dbFileName, 'utf8'));
     }
+    create(obj){
+        this._data.push(obj);
+        writeFile(this._dbFilename, JSON.stringify(this._data), 'utf8')
+    }
 }
 
 const db = new Db('client.json');
